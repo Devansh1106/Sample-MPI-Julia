3961c73471a9c1607c61bf0fd0ef3d111d5e8bba
Devansh1106 <devansh22@iisertvm.ac.in>
2024-03-27 23:38:23 +0530

    Code modified

--- test1.jl
+++ test1.jl
@@ -1,2 +1,18 @@
 using MPI
-MPI_Init()
+MPI.Init()
+
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
+println(rank)
+
+# println("Hello world $(rank) out of $(size)")
+buf::Vector{Int64} = zeros(1)
+if rank == 0
+    buf = [1]
+    MPI.Send(buf, 1, 10, comm)
+end
+if rank == 1
+    MPI.Recv!(buf, 0, 10, comm)  #MPI.Recv(buf, 0, 10, comm)---> Will not work. MPI.Recv is for receiving a single `isbits` objects
+    println(buf)
+end
