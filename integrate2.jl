486a1b05173e7415dcc6ff687c6bc9ed27eaeb2e
Devansh1106 <devansh22@iisertvm.ac.in>
2024-05-11 15:51:35 +0530

    Bug partially resolved

--- integrate2.jl
+++ integrate2.jl
@@ -1,5 +1,9 @@
 #integration of cos(x) has been performed
 
+# Currently due to a bug, code will give correct answer if used with n=1 process only.
+# Running on n>1 will return error.
+# Working on the issue...
+
 using MPI
 MPI.Init()
 
@@ -11,41 +15,55 @@ function integrate(a::Float64, b::Float64)
     return sin(b) - sin(a)
 end
 
-a, b = 0.0, 2.0
-mya = a + ((b-a)/size) * rank
+a, b = 0.0, 16.0
+mya = a + ((b-a)/size) * rank # # Check for errors
+# for i in 1:size-1
+#     if MPI.Test_cancelled(statuses[i])
+#         println("Request $i was cancelled")
+#     elseif MPI.Test_status(statuses[i])
+#         error_code = MPI.Error_code(statuses[i])
+#         println("Request $i completed with error code $error_code")
+#     else
+#         println("Request $i completed successfully")
+#     end
+# end
 myb = mya + (b-a)/size
 
 lres = integrate(mya, myb)
-# if rank == 1
-#     @show lres
-# end
-req = Vector{MPI.Request}(undef, size-1)
+
 if rank == 0
-    res = fill(0.0, size)
-    res[1] = lres
-    # @show res[1]
+    res = fill(0.0, size-1)
     req = Vector{MPI.Request}(undef, size-1)
 
     for i in 1:size-1
         req[i] = MPI.Irecv!(res, i, 0, comm)
     end
-    @show res
-    MPI.Waitall(req)
-    @show res
-
-    my_ans = 0.0
-    for j in 1:size
-        @show res
-        global my_ans += res[j]
-    end
-    println("Result of integration is $my_ans")
 else
-    # if rank == 2
-    #     @show lres
-    # end
     MPI.send(lres, 0, 0, comm)
 end
 
-MPI.Finalize()
+if rank == 0
+    my_ans = lres   # *
+    @show res
+    statuses = MPI.Waitall!(req)
+    @show statuses    # Waiting should be before the line in which communicated data need to be used
 
+    # # Check for errors
+    # for i in 1:size-1
+    #     if MPI.Test_cancelled(statuses[i])
+    #         println("Request $i was cancelled")
+    #     elseif MPI.Test_status(statuses[i])
+    #         error_code = MPI.Error_code(statuses[i])
+    #         println("Request $i completed with error code $error_code")
+    #     else
+    #         println("Request $i completed successfully")
+    #     end
+    # end
 
+    for j in 1:size-1
+        global my_ans += res[j] # global otherwise loop's scope will create it own my_ans variable instead of using * one
+    end
+    println("Result of integration is $my_ans")
+end
+
+# MPI.Finalize()    # Not needed in julia (also in tmux it will create a problem in running the program more than once in a single session)
