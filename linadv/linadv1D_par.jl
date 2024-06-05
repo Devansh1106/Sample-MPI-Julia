c6b0a54df0653d6bf232ebd665c8489ab5190044
Devansh1106 <devansh22@iisertvm.ac.in>
2024-06-05 15:25:09 +0530

    MPI.Scatter! removed

--- linadv/linadv1D_par.jl
+++ linadv/linadv1D_par.jl
@@ -27,30 +27,24 @@ else
 end
 
 N = parse(Int, ARGS[1])         # N = number of grid points
+@assert N % size == 0
 t = parse(Int, ARGS[2])         # t = final time
 cfl = parse(Float64, ARGS[3])
 a = parse(Int, ARGS[4])         # velocity
 
 xmin, xmax = 0.0, 1.0           # [xmin, xmax]
-N_local = div(N, size)          # / -> converts N to float; div() doesn't;   change in error_cal also
-x = nothing
+N_local = div(N, size)          # / -> converts N to float; div() doesn't
 dx = (xmax - xmin)/(N - 1)
-if rank == 0
-    x = fill(0.0, N)            # grid array
-    for i in 1:N
-        x[i] = xmin + (i-1)*dx
-    end
-end
+xmin_local = dx * rank * N_local
+xmax_local = xmin_local + dx * N_local
 
 x_local = fill(0.0, N_local)
-MPI.Scatter!(x, x_local, comm, root=0)
+for i in 1:N_local
+    x_local[i] = xmin_local + (i-1)*dx
+end
 
-grid_per_rank = (xmax - xmin)/size
-xmin = rank*grid_per_rank
-xmax = xmin + grid_per_rank
-
-@show N_local, t, xmin, xmax, a, cfl, dx
-param = (; N_local, t, dx, xmin, a, cfl)
+@show N_local, t, xmin_local, xmax_local, a, cfl, dx
+param = (; N_local, t, dx, a, cfl)
 
 
 # To generate a initial solution through initial condition
@@ -127,15 +121,15 @@ function solver(param)
     j = 0.0
     it = 0.0
 
-    dt = param.cfl * dx / abs(a)
-    sigma = abs(a) * dt / dx        # as a substitute to cfl
+    dt = param.cfl * param.dx / abs(param.a)
+    sigma = abs(a) * dt / param.dx        # as a substitute to cfl
 
     while j < t
         # -------Crucial block-------------
         if j + dt > t
-            dt = t - j                  # if `j + dt` goes beyond `t` then loop will quit in next iteration hence solution will not get calculated till `t`
+            dt = t - j                      # if `j + dt` goes beyond `t` then loop will quit in next iteration hence solution will not get calculated till `t`
                                             # With this, solution will get calculated as close to `t`
-            sigma = dt * abs(a) / dx
+            sigma = dt * abs(param.a) / param.dx
         end
         # ---------------------------------
 
