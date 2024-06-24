2dcfacfcf8cb5755207e715e545f31424f744bb8
Devansh1106 <devansh22@iisertvm.ac.in>
2024-06-24 15:12:12 +0530

    Interactive plots added in .html files

--- linadv/plot_serial2D.jl
+++ linadv/plot_serial2D.jl
@@ -1,6 +1,6 @@
 using Plots
 using DelimitedFiles
-
+plotlyjs()
 num_data = readdlm("num_sol2D.txt", Float64)  
 exact_data = readdlm("exact_sol2D.txt", Float64)
 
@@ -8,11 +8,11 @@ plot(num_data[:,1],num_data[:,2],num_data[:,3:end],
      label="Numerical Solution", st=:surface, xlabel="x", ylabel="y",
      title="Solution Plot", zlabel="Numerical solution",dpi=150)
 
-savefig("linadv2D_ser_num.png")
+savefig("linadv2D_ser_num.html")
 
 plot(exact_data[:,1],exact_data[:,2], exact_data[:,3:end],
       label="Exact Solution", xlabel="x", ylabel="y", zlabel="Exact solution",
       title="Solution Plot", st=:surface, dpi=150)
 
-savefig("linadv2D_ser_exact.png")
-println("Plot is in `linadv2D_ser_exact.png` and `linadv2D_ser_num.png` file")
+savefig("linadv2D_ser_exact.html")
+println("Plot is in `linadv2D_ser_exact.html` and `linadv2D_ser_num.html` file")
